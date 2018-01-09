00bfc46390f37122c796e75392bac452928f8e61
l.klimowicz <klimuseq@gmail.com>
2018-01-09 20:21:51 +0100

    first commit

---
+++ src/main/java/pl/ai/gym/GymApplication.java
@@ -0,0 +1,12 @@
+package pl.ai.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApplication.class, args);
+	}
+}
