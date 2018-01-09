9ce0b4d60dbb4a4a6f83dd2bb49ac9bfd873a93c
l.klimowicz <klimuseq@gmail.com>
2018-01-09 20:32:57 +0100

    commit

---
+++ src/main/java/pl/ai/gym/model/Client.java
@@ -0,0 +1,39 @@
+package pl.ai.gym.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+
+/**
+ * Created by Łukasz Klimowicz.
+ */
+
+@Entity
+@Table(name = "KLIENT")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Client {
+        @Id
+        @Column(name ="ID_CLIENT")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long clientId;
+
+        @Column(name ="NAME")
+        private String clientName;
+
+        @Column(name ="SURNAME")
+        private String clientSurname;
+
+        @Column(name ="PHONE")
+        private String clientPhone;
+
+        @ManyToOne(fetch = FetchType.EAGER)
+        @JoinColumn(name = "ID_GYM")
+        private Gym gym;
+    }
+
+}
